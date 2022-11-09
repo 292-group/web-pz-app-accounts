ff0b4479c351cf2115715f248d6a2e56ad9f04bc
Heorhii <georgii.ua@gmail.com>
2022-11-09 11:43:47 +0200

    (fix) final version

--- app/src/components/Accounts/Accounts.js
+++ app/src/components/Accounts/Accounts.js
@@ -7,7 +7,6 @@ import {Link, NavLink} from "react-router-dom";
 
 const Accounts = () => {
   const [modalActive, setModalActive]= useState(false)
-  const [current, updateCurrent] = useState({ name: "", account: "", status:"Active", email:"", startDate:"", expDate:""})
   const [mode, setMode] = useState()
   const [total, setTotal] = useState()
 
@@ -15,7 +14,17 @@ const Accounts = () => {
   const [isLoaded, setIsLoaded] = useState(false);
   const [items, setItems] = useState([]);
   const [itemm, setItemm] = useState([]);
-
+  function timeConverter(UNIX_timestamp){
+    if (UNIX_timestamp.indexOf('-') > -1) return UNIX_timestamp;
+    else{
+      let a = new Date(UNIX_timestamp * 1000);
+      let year = a.getFullYear();
+      let month = a.getMonth()+1;
+      let date = a.getDate();
+      let time = year + '-' + month + '-' + date;
+      return time;
+    }
+  }
   function get(){
     fetch("/accounts")
       .then(res => res.json())
@@ -45,7 +54,6 @@ const Accounts = () => {
         <div className={"accounts-holder__title"}>
           <h1 className={"title"}>Account list</h1>
           <button className={"create-account"} onClick={()=> {setModalActive(true)
-            updateCurrent( { name: "", account: "", status:"Active", email:"", startDate:"", expDate:""});
             setMode("create")
           }}>Create account</button>
         </div>
@@ -53,6 +61,7 @@ const Accounts = () => {
           <p>Total accounts: {total}</p>
         </div>
         <table className={"table"}>
+          <thead>
           <tr>
             <th>Name</th>
             <th>Account name</th>
@@ -62,34 +71,36 @@ const Accounts = () => {
             <th>Expiration date</th>
             <th></th>
           </tr>
-            {items.map(item => (
-              <tr>
-                <td><NavLink to={PROFILE_ROUTE+"/"+item.id} className={"profile"}><p>{item.name}</p></NavLink></td>
-                <td>{item.account_name}</td>
-                <td>{item.email}</td>
-                <td className={item.status=="Active"?"blue":item.status=="Pending"?"red":"yellow"}><div className={"status"}><p>{item.status}</p></div></td>
-                <td>{moment(item.start_date*1000).format('DD MMM YYYY')}</td>
-                <td>{moment(item.expiration_date*1000).format('DD MMM YYYY')}</td>
-                <td><div className={"buttons"}>
-                  <button className={'edit'} onClick={()=>{
+          </thead>
+      <tbody>
+      {items.map(item => (
+        <tr key={item.id}>
+          <td><NavLink to={PROFILE_ROUTE+"/"+item.id} className={"profile"}><p>{item.name}</p></NavLink></td>
+          <td>{item.account_name}</td>
+          <td>{item.email}</td>
+          <td className={item.status=="Active"?"blue":item.status=="Pending"?"red":"yellow"}><div className={"status"}><p>{item.status}</p></div></td>
 
-                    setMode("edit")
-                    setModalActive(true)
-                    setItemm(item)
-                    updateCurrent( { name: item.name, account: item.account_name, status:item.status, email:item.email, startDate:"", expDate:""});
-                    console.log(itemm)
-                  }
-                }>Edit</button>
-                  <button className={'delete'} onClick={()=>{
-                  fetch('/accounts/'+item.id, {
-                    method: 'DELETE'
-                  })
-                    get()
-                }}>Delete</button></div></td>
-              </tr>
-            ))}
+          <td>{moment(item.start_date*1000).format('DD MMM YYYY')}</td>
+          <td>{moment(item.expiration_date*1000).format('DD MMM YYYY')}</td>
+          <td><div className={"buttons"}>
+            <button className={'edit'} onClick={()=>{
+              setMode("edit")
+              setModalActive(true)
+              setItemm(item)
+            }
+            }>Edit</button>
+            <button className={'delete'} onClick={()=>{
+              fetch('/accounts/'+item.id, {
+                method: 'DELETE'
+              })
+              get()
+            }}>Delete</button></div></td>
+        </tr>
+      ))}
+      </tbody>
+
         </table>
-        <Modal active={modalActive} setActive={setModalActive} get={get} item={itemm} mode={mode} current={current}/>
+        <Modal active={modalActive} setActive={setModalActive} get={get} item={itemm} mode={mode}/>
       </div>
     );
   }
--- app/src/components/Modal/Modal.js
+++ app/src/components/Modal/Modal.js
@@ -2,19 +2,40 @@ import React, {useEffect, useReducer, useState} from 'react';
 import "./Modal.css"
 import moment from "moment";
 
-const Modal = ({active, setActive, get, item, mode, current}) => {
-  const initialState = current;
-  const [state, updateState] = useReducer(
-    (state, updates) => ({ ...state, ...updates }),
-    initialState
-  );
+const Modal = ({active, setActive, get, item, mode}) => {
+  let nameValue=React.createRef();
+  let accountValue=React.createRef();
+  let emailValue=React.createRef();
+  let exampleValue=React.createRef()
+  let dataStartValue=React.createRef();
+  let dataEndValue=React.createRef();
+  let id;
 
+useEffect(()=>{
+  if(mode==="edit"){
+    nameValue.current.value = item.name;
+    accountValue.current.value = item.account_name;
+    emailValue.current.value = item.email;
+    exampleValue.current.value = item.status;
+    dataStartValue.current.value = moment(item.start_date*1000).format('YYYY-MM-DD');
+    dataEndValue.current.value = moment(item.expiration_date*1000).format('YYYY-MM-DD');
+    id = item.id;
+  }
+  if(mode==="create"){
+    nameValue.current.value = "";
+    accountValue.current.value = "";
+    emailValue.current.value = "";
+    exampleValue.current.value = "Active";
+    dataStartValue.current.value = "";
+    dataEndValue.current.value = "";
+  }
+},[item, mode])
 
   function close(){
     setActive(false)
+    item = {}
   }
-  function post(e, setActive, state){
-    e.preventDefault()
+  function post(e, setActive){
     try {
       fetch('/accounts', {
         method: 'POST',
@@ -23,82 +44,70 @@ const Modal = ({active, setActive, get, item, mode, current}) => {
           'Content-Type': 'application/json'
         },
         body: JSON.stringify( {
-          "name": state.name,
-          "account_name": state.account,
-          "status": state.status,
-          "email": state.email,
-          "start-date": state.startDate,
-          "exp-date": state.expDate
+          "name": nameValue.current.value,
+          "account_name": accountValue.current.value,
+          "status": exampleValue.current.value,
+          "email": emailValue.current.value,
+          "start_date": new Date(dataStartValue.current.value).getTime() / 1000,
+          "expiration_date": new Date(dataEndValue.current.value).getTime() / 1000
         })
       }).then(res => res.json())
         .then(res => {
+
           get();
           setActive(false)
-         updateState({ name: "", account: "", status:"Active", email:"", startDate:"", expDate:""})
         });
     }catch (e){
       console.log(e.message)
     }
   }
-  function put(e, setActive, state){
+  function put(e, setActive){
     e.preventDefault()
-    try {
-      fetch(`/accounts/${item.id}`, {
-        method: 'PUT',
-        headers: {
-          'Accept': 'application/json, text/plain, */*',
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify( {
-          "name": state.name,
-          "account_name": state.account,
-          "status": state.status,
-          "email": state.email,
-          "start-date": state.startDate,
-          "exp-date": state.expDate
-        })
-      }).then(res => res.json())
-        .then(res => {
-          get();
-          setActive(false)
-          //updateState({ name: "", account: "", status:"Active", email:"", startDate:"", expDate:""})
-      });
-    }catch (e){
-      console.log(e.message)
-    }
+      try {
+        fetch(`/accounts/${id}`, {
+          method: 'PUT',
+          headers: {
+            'Accept': 'application/json, text/plain, */*',
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify( {
+            "name": nameValue.current.value,
+            "account_name": accountValue.current.value,
+            "status": exampleValue.current.value,
+            "email": emailValue.current.value,
+            "start_date": new Date(dataStartValue.current.value).getTime() / 1000,
+            "expiration_date": new Date(dataEndValue.current.value).getTime() / 1000
+          })
+        }).then(res => res.json())
+          .then(res => {
+            get();
+            setActive(false)
+          });
+      }catch (e){
+        console.log(e.message)
+      }
+
   }
-  function isEmpty(obj) {
-    for(let key in obj)
-    {
-      return false;
-    }
-    return true;
-  }
-  // useEffect(()=>{
-  //   console.log(item)
-  //   if(!isEmpty(item))
-  //   updateState({ name: item.name, account: item.account, status:item.status, email:item.email, startDate:item.startDate, expDate:item.expDate})
-  // },[])
 
   return (
-    <div className={active ?"modal active":"modal"} onClick={()=> setActive(false)}>
+    <div className={active ?"modal active":"modal"} onClick={()=> {setActive(false)}}>
       <div className={"modal__content"} onClick={e=> e.stopPropagation()}>
         <div className={"content-type"}>
-          <h1 onClick={()=>console.log(current)}>{mode==="create"?"Create account":"Edit profile"}</h1>
+          <h1>{mode==="create"?"Create account":"Edit profile"}</h1>
           <div className={"close"} onClick={()=> setActive(false)}>X</div>
         </div>
-        <form action="" method="post" className="form-sub">
+        <form className="form-sub">
           <div className="form-input">
             <label htmlFor="name">Name: </label>
-            <input type="text" name="name" id="name" defaultValue={mode==="create"?state.name:item.name} onChange={e => updateState({name:e.target.value})} required/>
+            <input type="text" name="name" id="name" ref={nameValue} required/>
           </div>
           <div className="form-input">
             <label htmlFor="name">Account name: </label>
-            <input type="text" name="account-name" id="name" defaultValue={mode==="create"?state.account:item.account_name} onInput={e => updateState({account:e.target.value})} required/>
+            <input type="text" name="account-name" id="name" ref={accountValue} required/>
           </div>
           <div className="form-input">
             <label htmlFor="status">Status: </label>
-            <select name="status" value={mode==="create"?"Active":item.status} onChange={e => updateState({status:e.target.value})}>
+            <select name="status" ref={exampleValue}>
               <option value="Active">Active</option>
               <option value="Pending">Pending</option>
               <option value="Disable">Disable</option>
@@ -106,21 +115,21 @@ const Modal = ({active, setActive, get, item, mode, current}) => {
           </div>
           <div className="form-input">
             <label htmlFor="email">Email: </label>
-            <input type="email" name="email" id="email" defaultValue={mode==="create"?state.email:item.email} onInput={e => updateState({email:e.target.value})} required/>
+            <input type="email" name="email" id="email" ref={emailValue} required/>
           </div>
           <div className="form-date">
             <div className={"form-date__block"}>
               <label htmlFor="start-date">Start date: </label>
-              <input type="date" name="start-date" id="start-date" defaultValue={mode==="create"?"":moment(item.start_date*1000)} onChange={e => updateState({startDate:moment(e.target.value, "DD MMM YYYY").format("X")})} required/>
+              <input type="date" name="start-date" id="start-date"ref={dataStartValue} required/>
             </div>
            <div className={"form-date__block"}>
              <label htmlFor="exp-date">Expiration date: </label>
-             <input type="date" name="exp-date" id="exp-date" onChange={e => updateState({expDate:moment(e.target.value, "DD MMM YYYY").format("X")})} required/>
+             <input type="date" name="exp-date" id="exp-date" ref={dataEndValue} required/>
            </div>
           </div>
           <div className="form-send">
-            <button type="" className={'confirm'} onClick={ e =>{
-              mode==="create"?post(e, setActive, state):put(e,setActive, state)}}>{mode==="create"?"Create":"Update"}</button>
+            <input type="submit" className={'confirm'} onClick={ e =>{
+              mode==="create"?post(e, setActive):put(e,setActive)}} value={mode==="create"?"Create":"Update"}></input>
             <button className={'close'} onClick={close}>Cancel</button>
           </div>
         </form>
