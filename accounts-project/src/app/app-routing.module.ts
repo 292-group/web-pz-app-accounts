4a1a2831897447519e8dfd742c4a478460d47c73
UriiChe <cherkun.ura@gmail.com>
2023-06-06 20:51:49 +0300

    NO-ISSUE: put components to app template, set up modiles, routes

--- accounts-project/src/app/app-routing.module.ts
+++ accounts-project/src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { APP_ROUTES } from './app.constants';
+import { AccountListComponent } from './account-list/account-list.component';
+import { AccountComponent } from './account/account.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: APP_ROUTES.ACCOUNTS,
+    component: AccountListComponent,
+},
+{
+    path: `${APP_ROUTES.PROFILE}/:id`,
+    component: AccountComponent,
+},
+{
+    path: APP_ROUTES.ABOUT_US,
+    component: AboutUsComponent
+},
+{
+  path: '**',
+  redirectTo: APP_ROUTES.ACCOUNTS
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- accounts-project/src/app/app.module.ts
+++ accounts-project/src/app/app.module.ts
@@ -7,6 +7,24 @@ import { AccountListComponent } from './account-list/account-list.component';
 import { AccountComponent } from './account/account.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { AccountDialogueComponent } from './account-dialogue/account-dialogue.component';
+import { MatTabsModule } from '@angular/material/tabs';
+import { APP_BASE_HREF, DatePipe } from '@angular/common';
+import { environment } from 'src/environments/environment';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatTableModule } from '@angular/material/table';
+import { HttpClientModule } from '@angular/common/http';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+import { DatePickerComponent } from './account-dialogue/date-picker.component';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { TextInputComponent } from './account-dialogue/text-input.component';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatListModule} from '@angular/material/list';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -14,13 +32,34 @@ import { AccountDialogueComponent } from './account-dialogue/account-dialogue.co
     AccountListComponent,
     AccountComponent,
     AboutUsComponent,
-    AccountDialogueComponent
+    AccountDialogueComponent,
+    TextInputComponent,
+    DatePickerComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatTabsModule,
+    BrowserAnimationsModule,
+    MatDialogModule,
+    MatTableModule,
+    HttpClientModule,
+    MatButtonModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatDividerModule,
+    MatListModule,
+    MatIconModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    { provide: APP_BASE_HREF, useValue: '/' },
+    { provide: 'API_URL', useValue: environment.serverUrl },
+    DatePipe
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
